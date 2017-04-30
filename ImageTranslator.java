59f28f66f3beaeacd861f4a0fe30a453f0d8294c
= <asen.nikolaev95@abv.bg>
2017-04-30 15:13:01 +0300

    wrong file

--- ImageTranslator.java
+++ ImageTranslator.java
@@ -14,40 +14,12 @@ public class ImageTranslator {
 	public static void main(String[] args) throws IOException {
 
 		ImageTranslator translator = new ImageTranslator();
-		// translator.translateImageToBIN(FILE_PATH);
-		translator.convertTheImage(3);
+		translator.translateImageToBIN(FILE_PATH);
+	
 
 	}
 
-	private void convertTheImage(int bites) throws IOException {
-		BufferedReader st = null;
-
-		File newFile = new File("created.png");
-		if (newFile.createNewFile())
-			;
-		BufferedOutputStream stg = new BufferedOutputStream(new FileOutputStream(newFile));
-
-		st = new BufferedReader(new FileReader(new File(FILE_PATH)), 1024);
-		String line = st.readLine();
-		while (line != null) {
-			for (int i = 0; i < line.length(); i++) {
-				System.out.print(Integer.toBinaryString((int) (line.charAt(i))) + " ");
-				char bit = line.charAt(i);
-				bit += bites;
-				stg.write((int) bit);
-				stg.flush();
-			}
-			line = st.readLine();
-			stg.write((int) '\n');
-			stg.flush();
-			System.out.println();
-
-		}
-		st.close();
-		stg.close();
-		System.out.println("DONE");
-	}
-
+	
 	private void translateImageToBIN(String filePath) throws IOException {
 		BufferedReader st = null;
 		st = new BufferedReader(new FileReader(new File(FILE_PATH)), 1024);
